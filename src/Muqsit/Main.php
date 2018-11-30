9642c0c3f64b65a51eef034bf4292e030aedc837
iZeaoGamer <jameshayward14@icloud.com>
2018-11-30 13:56:33 +0000

    Update Main.php

--- src/Muqsit/Main.php
+++ src/Muqsit/Main.php
@@ -4,7 +4,7 @@ namespace Muqsit;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\utils\Config;
+use pocketmine\utils\{TextFormat, Config};
 use onebone\economyapi\EconomyAPI;
 use pocketmine\math\Vector3;
 
@@ -19,13 +19,16 @@ class Main extends PluginBase implements Listener{
   
   public function mine(BlockBreakEvent $e){
     $p = $e->getPlayer();
+    $player = $e->getPlayer()->getName();
     if($p->hasPermission("miningmoney.allow")){
       $cfg = $this->getConfig();
       $block = $e->getBlock();
+      $blockName = $e->getBlock()->getName();
       $message = $cfg->get("reward-message");
       $price = $cfg->get($block->getId());
         if($price !== null && $price > 0){
           EconomyAPI::getInstance()->addMoney($p, $price);
+          $this->getServer()->broadcastMessage(TextFormat::DARK_PURPLE . $player . " §dhas mined§5 $blockName §dand has gained §5$price");
           $p->sendMessage(str_replace("{reward}", $price, $message));
         }
     }
